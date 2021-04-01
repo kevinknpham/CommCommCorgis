a5e6d0dee4f8456204d0dd4ee882831613f3388a
miguel <74852114+mickeygcodes@users.noreply.github.com>
2021-03-31 17:29:57 -0700

    Add form to index and modified login to make it work

--- static/js/login.js
+++ static/js/login.js
@@ -1,13 +1,15 @@
+const form = document.getElementById("form");
+form.addEventListener("submit", login());
+
 function login() {
   const input = document.getElementById("login-input");
   const loginBtn = document.getElementById("login-button");
   input.addEventListener("input", function (e) {
     loginBtn.disabled = e.target.value.length === 0;
   });
-  loginBtn.addEventListener("click", function () {
-    const username = document.getElementById("login-input").value;
-    window.location.href = "main.html";
-  });
+  const username = document.getElementById("login-input").value;
+  // loginBtn.addEventListener("click", function () {
+  //   const username = document.getElementById("login-input").value;
+  //   window.location.href = "main.html";
+  // });
 }
-
-login();
