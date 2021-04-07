891a4d552ba38ada7f8a2a3cadacb9a812ce10e2
warandstar <andreakim123@naver.com>
2021-04-06 23:10:48 -0700

    Trying to implement update character

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,13 @@
 "use strict";
 
 // - mouse is clicked (function started by onClick)
+var charLength = 120;
 
 function moveCharacter(username) {
   var currentPosition = document.getElementById(username);
   var cursorX;
   var cursorY;
 
-  var charLength = 120;
 
   document.getElementById("myCanvas").onclick = (event) => {
     cursorX = event.clientX;
@@ -25,4 +25,12 @@ function moveCharacter(username) {
   // - function that does animation of character (div?) to that value
 
   // - done!
+}
+
+function handleUpdateChar(data) {
+  if (data.name && data.x && data.y) {
+    var currentPosition = document.getElementById(data.name);
+    currentPosition.style.left = `${data.x - charLength / 1.5}px`;
+    currentPosition.style.top = `${data.y - charLength / 1.5}px`;
+  }
 }
