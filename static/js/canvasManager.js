09a3482bf7e6a606f62a2f546baf1fa56023bc5b
miguel <74852114+mickeygcodes@users.noreply.github.com>
2021-06-15 23:57:39 -0700

    add name to image not working quite yet

--- static/js/canvasManager.js
+++ static/js/canvasManager.js
@@ -10,6 +10,17 @@ const COLOR_TO_URL = Object.freeze(
   ])
 );
 
+function drawNameOnImage(ctx, text, x, y) {
+  ctx.font = '50px monospace';
+  ctx.shadowColor = 'white';
+  ctx.shadowBlur = 7;
+  ctx.lineWidth = 5;
+  ctx.strokeText(text, x + 10, y);
+  ctx.shadowBlur = 0;
+  ctx.fillStyle = 'black';
+  ctx.fillText(text, x + 10, y);
+}
+
 function placeImage(url) {
   const image = new Image();
   image.src = url;
@@ -47,6 +58,7 @@ class CanvasManager {
     for (let i = 0; i < characterList.length; i++) {
       const name = characterList[i];
       const info = this.characterManager.getCharacterInfo(name);
+      drawNameOnImage(this.ctx, name, info.currentX, info.currentY);
       this.ctx.drawImage(
         placeImage(COLOR_TO_URL.get(info.attributes.color)),
         info.currentX,
